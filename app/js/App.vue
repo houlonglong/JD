986bb946c8ca543035380664712d4aac05a1082c
jack hou <31363537@qq.com>
2018-10-20 22:52:33 +0800

    首页头部组件，slider组件完成

--- app/js/App.vue
+++ app/js/App.vue
@@ -1,8 +1,14 @@
 <template>
-
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
+
 <script>
-export default {
-    
-}
+  export default {
+    name:'App'
+  }
 </script>
+
+<style style="scss">
+</style>
--- app/js/core/btn.vue
+++ app/js/core/btn.vue
@@ -22,9 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../../css/element.scss"
+@import '../../css/element.scss';
 .btn{
-  @inclue btn
+    @include btn;
 }
 </style>
 
